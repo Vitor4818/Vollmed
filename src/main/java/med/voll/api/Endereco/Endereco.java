aee4fe946fa9765e878b655de522c2d3fdda9d1e
Vitor4818 <vitorg.martins4818@gmail.com>
2025-04-07 22:06:02 -0300

    feat: criar entidade Medico e Endereco com mapeamento JPA

---
+++ src/main/java/med/voll/api/Endereco/Endereco.java
@@ -0,0 +1,20 @@
+package med.voll.api.Endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,27 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.Endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter //Usando lombok para criar metodos getters
+@NoArgsConstructor //Criando construtor sem parametros com lombok
+@AllArgsConstructor //Criando construtor com paramentros com lombok
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+    @Embedded
+    private Endereco endereco;
+}
